fb8a5fb00081c939a3f0ef7f48a980acba13ada6
August-IE <Aeromosele9@gmail.com>
2023-12-25 13:52:39 -0800

    puppet client config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,16 +2,21 @@
 # SSH client is configured to use the private key ~/.ssh/school
 # SSH client is configured to refuse to authenticate using a password
 
-file { '~/.ssh/config':
-  ensure => present,
+# Ensure the .ssh directory exists
+file { '/home/ubuntu/.ssh':
+  ensure => directory,
   owner  => 'ubuntu',
   group  => 'ubuntu',
-  mode   => '0600',
-  content => "
-    Host 271026-web-01
-        HostName 54.236.203.202
-        User ubuntu
-        IdentityFile ~/.ssh/school
-        PasswordAuthentication no
-  ",
+  mode   => '0700',
+}
+
+# Manage the SSH configuration file
+file_line { 'Turn off passwd auth':
+  path  => '/home/ubuntu/.ssh/config',
+  line  => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path  => '/home/ubuntu/.ssh/config',
+  line  => 'IdentityFile ~/.ssh/school',
 }
