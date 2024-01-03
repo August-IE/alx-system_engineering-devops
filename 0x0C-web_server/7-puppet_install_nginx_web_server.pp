27b556f0e88ad0137483bee43280d6edee320b6e
August-IE <Aeromosele9@gmail.com>
2024-01-03 14:08:04 -0800

    Nginx puppet installation

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,62 +3,17 @@
 # It must return a page that contains the string Hello World!
 # The redirection must be a “301 Moved Permanently”
 
-# Define a class for Nginx installation and configuration
-class nginx_config {
-
-  # Install Nginx package
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  # Ensure Nginx service is running and enabled
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => Package['nginx'],
-  }
-
-  # Configure Nginx main configuration file
-  file { '/etc/nginx/nginx.conf':
-    ensure  => file,
-    content => template('nginx/nginx.conf.erb'),
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-
-  # Create a virtual host file for the default site
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => template('nginx/default-site.erb'),
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-
-  # Create a symbolic link to enable the default site
-  file { '/etc/nginx/sites-enabled/default':
-    ensure  => link,
-    target  => '/etc/nginx/sites-available/default',
-    require => File['/etc/nginx/sites-available/default'],
-    notify  => Service['nginx'],
-  }
+package { 'nginx':
+provider => 'apt',
 }
+exec {'hlbtn_page':
+command => '/usr/bin/sudo /bin/echo Hello World! > /var/www/html/index.nginx-debian.html',
+}
+exec {'redirect_page':
 
-# Define a class for the 301 redirect
-class redirect_config {
+command => '/usr/bin/sudo /bin/sed -i "66i rewrite ^/redirect_me https://www.google.com/ permanent;" /etc/nginx/sites-available/default',
+}
+exec {'start_server':
 
-  # Create a location block for the redirect
-  file { '/etc/nginx/sites-available/redirect':
-    ensure  => file,
-    content => template('nginx/redirect-site.erb'),
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-
-  # Create a symbolic link to enable the redirect site
-  file { '/etc/nginx/sites-enabled/redirect':
-    ensure  => link,
-    target  => '/etc/nginx/sites-available/redirect',
-    require => File['/etc/nginx/sites-available/redirect'],
-    notify  => Service['nginx'],
-  }
+command => '/usr/bin/sudo /usr/sbin/service nginx start',
 }
