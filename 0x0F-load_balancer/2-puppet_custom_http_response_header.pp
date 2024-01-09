1f34d7c8ec4419c8a2ef80ae628fa54ba1157870
August-IE <Aeromosele9@gmail.com>
2024-01-08 16:59:28 -0800

    custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,37 +1,29 @@
 #!/usr/bin/env bash
 # A script that creates a custom HTTP header response, but with Puppet.
 
-package { 'nginx':
-  ensure => installed,
-}
-
-# Define Nginx configuration file
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/nginx.conf.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Define the Nginx service
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-}
-
-# Restart Nginx whenever the configuration changes
-exec { 'restart_nginx':
-  command     => '/usr/sbin/service nginx restart',
+exec { 'update_package_lists':
+  command  => 'sudo apt-get update',
+  logoutput => true,
   refreshonly => true,
-  subscribe   => File['/etc/nginx/sites-available/default'],
 }
 
-# Create a custom fact for the server hostname
-file { '/etc/facter/facts.d/server_hostname.txt':
-  ensure  => file,
-  content => "server_hostname=${::hostname}\n",
+package { 'nginx':
+  ensure => present,
+  require => Exec['update_package_lists'],
 }
 
-Service['nginx'] {
-  subscribe => File['/etc/facter/facts.d/server_hostname.txt'],
+file_line { 'custom_http_header':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "  add_header X-Served-By $hostname;",
+  match  => '^\s*location / {',
+  require => Package['nginx'],
+  notify => Exec['restart_nginx'],
 }
+
+exec { 'restart_nginx':
+  command  => 'sudo service nginx restart',
+  refreshonly => true,
+  subscribe => File_line['custom_http_header'],
+}
+
