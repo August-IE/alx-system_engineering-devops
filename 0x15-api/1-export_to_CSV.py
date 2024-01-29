6edf3d2500229147a13b55ed839ad6a41e9cfe67
August-IE <Aeromosele9@gmail.com>
2024-01-29 06:35:22 -0800

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A script that exports task #0 data in the CSV format."""
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    filename = "{}.csv".format(user_id)  # File name as USER_ID.csv
+
+    with open(filename, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for t in todos:
+            completed = "True" if t.get("completed") else "False"
+            title = t.get("title")
+            writer.writerow([user_id, username, completed, title])
