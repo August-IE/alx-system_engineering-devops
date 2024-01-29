a17bc0b396253dc7148a4a8a54990b25a0fe7438
August-IE <Aeromosele9@gmail.com>
2024-01-29 06:38:12 -0800

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """A script that exports task #0 data in the CSV format."""
+import csv
 import requests
 import sys
-import csv
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
