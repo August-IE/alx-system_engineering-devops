080981068be25a088844571cb671015a5b256abc
August-IE <Aeromosele9@gmail.com>
2024-02-15 03:30:39 -0800

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A function that queries the Reddit API and returns the number of
+subscribers for a given subreddit"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json().get("data", {})
+            return data.get("subscribers", 0)
+        elif response.status_code == 404:
+            return 0
+        else:
+            return 0  # Handle other error cases gracefully
+    except requests.exceptions.RequestException as e:
+        print("Error: {}".format(e))
+        return 0
