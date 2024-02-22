f5d39d3216d652cfe029c170b7d92da697145da2
August-IE <Aeromosele9@gmail.com>
2024-02-22 05:29:23 -0800

    Sky is the limit, let us bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,14 @@
 # A puppet script that increases the amount of traffic an Nginx server can handle.
 
 # Adjusting the ULIMIT setting of the default file
-exec { 'fix-for-nginx':
-  command => 'echo ULIMIT="-n 2000" > /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-  provider => 'shell'
+exec { 'change nginx ulimit':
+command  => 'echo ULIMIT="-n 2000" > /etc/default/nginx',
+path     => '/usr/bin',
+provider => 'shell'
 }
 # Restart Nginx in order to apply changes
 exec { 'restart nginx':
-  command => 'service nginx restart',
-  path    => '/usr/local/bin/:/bin/',
-  provider => 'shell'
+command  => 'service nginx restart',
+path     => '/usr/bin',
+provider => 'shell'
 }
