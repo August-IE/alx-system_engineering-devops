cd1f6b6171710d9c47ebe5a9585a62cdaa576754
August-IE <Aeromosele9@gmail.com>
2024-02-22 05:45:02 -0800

    one last try

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,14 @@
 # A puppet script that increases the amount of traffic an Nginx server can handle.
 
 # Adjusting the ULIMIT setting of the default file
-exec { 'fix--for-nginx':
+exec { 'fix-for-nginx':
   command => 'echo ULIMIT="-n 2000" > /etc/default/nginx', 
-  path     => '/usr/bin',
+  path     => '/usr/local/bin/:/bin/',
   provider => 'shell'
 }
 # Restart Nginx in order to apply changes
 exec { 'nginx-restart':
   command  => 'nginx restart',
-  path     => '/usr/bin',
+  path     => '/usr/local/bin/:/bin/'
   provider => 'shell'
 }
