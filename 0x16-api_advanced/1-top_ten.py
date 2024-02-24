bf3bde0bbf8958a72993180bffa5a44e110a1d5c
August-IE <Aeromosele9@gmail.com>
2024-02-24 03:30:11 -0800

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
-import json
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts listed for a given subreddit.
+    """Returns a list of titles of all hot posts on a given subreddit."""
 
-    Args:
-        subreddit (str): The name of the subreddit.
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+        return
 
-    Returns:
-        None
-    """
+    user_agent = {'User-agent': 'Custom User Agent'}
+    params = {'limit': 10}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'MyRedditBot/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json().get('data').get('children')
+    try:
+        response = requests.get(url, headers=user_agent, params=params)
+        response.raise_for_status()  # Raise an error for bad responses
+        data = response.json().get('data', {}).get('children', [])
+
         for post in data:
             print(post['data']['title'])
-    else:
+
+    except requests.exceptions.RequestException as e:
+        print("Error: {}".format(e))
         print("None")
