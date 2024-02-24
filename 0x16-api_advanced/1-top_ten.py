097a932b15083bb0cc816f805aaf0d8538cda569
August-IE <Aeromosele9@gmail.com>
2024-02-24 03:49:33 -0800

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 """Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
+import sys
 
 
 def top_ten(subreddit):
     """Returns a list of titles of all hot posts on a given subreddit."""
-
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
-        return
-
-    user_agent = {'User-agent': 'Custom User Agent'}
-    params = {'limit': 10}
+    headers = {'User-agent': 'Custom User Agent'}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-
+    response = requests.get(url, headers=headers)
     try:
-        response = requests.get(url, headers=user_agent, params=params)
-        response.raise_for_status()  # Raise an error for bad responses
-        data = response.json().get('data', {}).get('children', [])
-
-        for post in data:
-            print(post['data']['title'])
-
-    except requests.exceptions.RequestException as e:
-        print("Error: {}".format(e))
-        print("None")
+        # Check if the response status code is OK (200)
+        if response.status_code == 200:
+            # Try to decode the JSON response
+            response_json = response.json()
+            # Check if the response contains data and children
+            if 'data' in response_json and 'children' in response_json['data']:
+                top_ten_posts = response_json['data']['children']
+                for post in top_ten_posts:
+                    print("{}".format(post['data']['title']))
+            else:
+                print("No posts found.")
+        else:
+            print("Failed. Status code: {}".format(response.status_code))
+            print("Response content: {}".format(response.content))
+    except Exception as e:
+        print("Error: {}".format(str(e)))
