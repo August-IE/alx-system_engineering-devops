9d8042124b6165246ae09fadcdce5ede00ed41f0
August-IE <Aeromosele9@gmail.com>
2024-02-24 02:27:52 -0800

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,11 @@ import requests
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+        "User-Agent": "Custom User Agent"}
 
     response = requests.get(url, headers=headers)
 
-    if response.status_code == 404:
+    if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
